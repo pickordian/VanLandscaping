0a80b8f1144589abd164148675514a7d00941a6b
pickordian <pxtran7@gmail.com>
2024-01-31 06:08:55 -0600

    quick minor fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState} from "react";
 import { setNotification } from "./Notice";
 function Contact() {
   const [name, setName] = useState<string>("");
