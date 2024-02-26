760cd7d11568969aad7b988c5d617d254ed0c4b8
pickordian <pxtran7@gmail.com>
2024-02-26 02:52:38 -0600

    adjust color for better contrast and aria role for nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,19 +13,19 @@ function Header() {
       </a>
       <BurgerMenu state={isNavOpened} setState={setNavOpened} />
       <ul id="menu" role="menu" aria-labelledby="mobile-menu-toggle"className={isNavOpened ? "opened" : ""}>
-        <li role="navigation-menu-item">
+        <li role="menuitem">
           {" "}
           <a href="#Services" onClick={handleLinkClick}>Services</a>
         </li>
-        <li role="navigation-menu-item">
+        <li role="menuitem">
           {" "}
           <a href="#Gallery" onClick={handleLinkClick}>Gallery</a>
         </li>
-        <li role="navigation-menu-item">
+        <li role="menuitem">
           {" "}
           <a href="#Contact" onClick={handleLinkClick}>Contact</a>
         </li>
-        <li role="navigation-menu-item">
+        <li role="menuitem">
           {" "}
           <a className="CTA-link" href="tel:+14252453647">
             Call us: (425) 245-3647{" "}
